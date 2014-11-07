64557eebd846466edb86f14f3e1abccff64d6bb0
unknown <rechen@697-334-Frodo.sf.k12.internal>
2014-11-07 11:53:25 -0800

    first asteroids

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip rebecca= new SpaceShip();
 Stars[] manyStars = new Stars[40];
 Asteroids[] cluster=new Asteroids[20];
@@ -35,11 +51,11 @@ Asteroids[] cluster=new Asteroids[20];
       {
         if(keyCode==UP)
         {
-         rebecca.accelerate(.1);
+         rebecca.accelerate(.1f);
        }
        if(keyCode==DOWN)
        {
-        rebecca.accelerate(-.1);
+        rebecca.accelerate(-.1f);
        }
        if(keyCode==RIGHT)
        {
@@ -236,3 +252,12 @@ Asteroids[] cluster=new Asteroids[20];
     }   
   } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
